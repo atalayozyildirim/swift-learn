a89bbf5f17c1ff316853891b7a5702f085aaf0e3
atalayozyildirim <atalayozyildirim@gmail.com>
2025-02-26 17:23:17 +0300

    added new page

---
+++ test-x/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  test-x
+//
+//  Created by Atalay Özyıldırım on 26.02.2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
